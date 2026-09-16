df6f64f6eae6cee7afbc6cca4a6b96e1840c1b97
Von Baron Saenz <vonbaronsaenz@gmail.com>
2026-09-16 17:01:47 +0000

    feat: function is prime

--- src/main.rs
+++ src/main.rs
@@ -7,3 +7,21 @@ fn main() {
     println!("{}", is_even(4)); //output: true
     println!("{}", is_even(7)); //output: false
 }
+
+
+fn is_prime(n: i32) -> bool }
+   if n < 2 {
+      return false;
+   }
+
+   for i in 2..n { 
+       if n % i == 0 {
+          return false;
+       }
+
+   true
+   }
+  
+fn main() { 
+println!("{}", is_prime(7)); // Prints: true
+}
